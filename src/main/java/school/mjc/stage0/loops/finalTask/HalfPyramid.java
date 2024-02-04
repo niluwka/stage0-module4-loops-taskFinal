8f5f1f782edc9911c4f8456b5cb9aab5c40afddb
niluwka0403 <ninimamatova0403@gmail.com>
2024-02-04 14:52:12 +0500

    Final Task

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1 ; i <= cathetusLength; i++) {
+            for (int m = 1; m <= cathetusLength - i ;m++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1 ; k--) {
+                System.out.print("*");
+            }
 
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1 ; i <= cathetusLength; i++) {
+            for (int m = 1; m <= cathetusLength - i ;m++) {
+                    System.out.print(" ");
+            }
+            for (int k = i; k >= 1 ; k--) {
+                System.out.print( k);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print( j);
+            }
+            System.out.println();
+        }
 
     }
 
